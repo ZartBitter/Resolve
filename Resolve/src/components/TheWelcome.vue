2730f119b49e1e54b65d61f3eb415180e3def32b
Oğuz <oguz651@hotmail.de>
2022-02-20 21:16:28 +0100

    ComponentTest2

--- Resolve/src/components/TheWelcome.vue
+++ Resolve/src/components/TheWelcome.vue
@@ -1,13 +1,17 @@
 <script setup>
 import HomepageRoad from './icons/HomepageRoad.vue'
+import HomepageThoughts from './icons/HomepageThoughts.vue'
 </script>
 
 <template>
  
-  <div style="background: linear-gradient(112.12deg, rgba(255, 255, 255, 0.48) 0.51%, rgba(255, 255, 255, 0.12) 100%); backdrop-filter: blur(100px); border-radius: 50%;">
+ <div>
+  <div class="mt-4" style="background: linear-gradient(112.12deg, rgba(255, 255, 255, 0.48) 0.51%, rgba(255, 255, 255, 0.12) 100%); backdrop-filter: blur(100px); border-radius: 50%;">
     <HomepageRoad></HomepageRoad>
   </div>
-
+  
+  <HomepageThoughts></HomepageThoughts>
+</div>
   
 
 
