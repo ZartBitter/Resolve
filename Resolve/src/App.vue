e111029b5bc7b3674259d2d98eda6d7352806627
Oğuz <oguz651@hotmail.de>
2022-02-22 19:39:33 +0100

    CleanUp

--- Resolve/src/App.vue
+++ Resolve/src/App.vue
@@ -1,21 +1,20 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/Resolve.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <br>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
+
   </header>
 
   <RouterView />
