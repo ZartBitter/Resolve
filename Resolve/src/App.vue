2e02fd4a919a1614426c6795d8d93676dcff6c5f
Oğuz <oguz651@hotmail.de>
2022-02-22 20:57:03 +0100

    Homepage beginning

--- Resolve/src/App.vue
+++ Resolve/src/App.vue
@@ -4,7 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
+  <header class="font-serif">
     <img alt="Vue logo" class="logo" src="@/assets/Resolve.svg" width="125" height="125" />
       <nav>
         <RouterLink to="/">Home</RouterLink>
--- Resolve/src/components/TheWelcome.vue
+++ Resolve/src/components/TheWelcome.vue
@@ -1,17 +1,36 @@
 <script setup>
 import HomepageRoad from './icons/HomepageRoad.vue'
 import HomepageThoughts from './icons/HomepageThoughts.vue'
+import HomepageSuccess from './icons/HomepageSuccess.vue';
+import HomepageBuilding from './icons/HomepageBuilding.vue';
 </script>
 
 <template>
  
- <div>
-  <div class="mt-4" style="background: linear-gradient(112.12deg, rgba(255, 255, 255, 0.48) 0.51%, rgba(255, 255, 255, 0.12) 100%); backdrop-filter: blur(100px); border-radius: 50%;">
-    <HomepageRoad></HomepageRoad>
-  </div>
-  
-  <HomepageThoughts></HomepageThoughts>
-</div>
+    <!-- Upper Section of the Hompage -->
+    <section class="inline">
+      <HomepageRoad></HomepageRoad>
+      <HomepageThoughts></HomepageThoughts>
+    </section>
+
+    <!-- Login Card -->
+    <section>
+      <div class="p-6 max-w-sm mx-auto bg-gradient-to-r from-green-100 via-cyan-400 to-blue-600 hover:from- hover:to-purple-800 rounded-xl shadow-lg flex items-center space-x-4">
+        <img alt="Vue logo" class="logo" src="@/assets/Resolve.svg" width="100" height="100"/>
+      <div>
+       <button class="text-2xl font-medium text-indigo-800">Anmelden</button>
+       <p class="text-neutral-50">Noch keinen Account?</p>
+       <button class="text-sky-600">Registrieren</button>
+      </div>
+     </div>
+    </section>
+
+    <!-- Lower Section of the Homepage -->
+    <section>
+      <HomepageBuilding></HomepageBuilding>
+      <HomepageSuccess></HomepageSuccess>
+    </section>
+
   
 
 
