77485ad1432bc084552b47b6b561c967c72a2c22
Oğuz <oguz651@hotmail.de>
2022-03-02 19:17:53 +0100

    Basic responsive for UI

--- Resolve/src/App.vue
+++ Resolve/src/App.vue
@@ -7,9 +7,9 @@ import { RouterLink, RouterView } from 'vue-router'
   <!-- Circle in the left corner -->
   <div class="bg-gradient-to-br from-sky-600 to-green-50 h-4/5 w-2/5 rounded-full absolute blur-2xl -z-10 top-0 left-0"></div>
   <!-- Blurred circle in the right corner -->
-  <div class="bg-gradient-to-tr from-blue-600 to-green-100 h-4/5 w-2/5 rounded-full absolute blur-3xl -z-10 bottom-0 right-0"></div>
+  <div class="bg-gradient-to-tr from-blue-600 to-green-100 lg:h-4/5 lg:w-2/5 h-64 w-64 rounded-full absolute blur-3xl -z-10 lg:bottom-0 bottom-1/3 right-0"></div>
   <header class="font-serif">
-    <img alt="Vue logo" class="logo" src="@/assets/Resolve.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/Resolve.svg" width="125" height="125"/>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <br>
