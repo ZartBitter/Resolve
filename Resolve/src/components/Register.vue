db4198ac605f58e3b40021b42a01a8ea797d49ed
Oğuz <oguz651@hotmail.de>
2022-03-09 19:37:27 +0100

    y

--- Resolve/src/components/Register.vue
+++ Resolve/src/components/Register.vue
@@ -26,7 +26,7 @@ const register = () => {};
         <h1>Create an Account</h1>
         <p><input type="text" placeholder="Email" v-model="email" /></p>
         <p><input type="password" placeholder="Password" v-model="password" /></p>
-        <p><button @click="register">Submit</button></p>
+        <p><button @click="register">Registrieren</button></p>
       </template>
     </modal>
   </Teleport>
